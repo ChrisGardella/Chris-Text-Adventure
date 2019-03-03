1f5001e561146025263ede6e6c272d7494fe60cb
ChrisGardella <chrisgthe1st@gmail.com>
2019-03-03 13:25:23 -0500

    Added the introduction and start.

--- Chris's Text Adventure.py
+++ Chris's Text Adventure.py
@@ -1,1 +1,8 @@
-print ("Welcome to the best text adventure ever!")
+print ("You have traveled a long way to find a temple you saw in a dream.... and it was true! You see the door in front of you.")
+print ("Do you wish to enter?")
+answer = input()
+if answer == "yes":
+    print("You chose to go in...")
+    #Here will be the character generator to tell you your stats
+if answer == "no":
+    print("You chose to run away like a whimp")
