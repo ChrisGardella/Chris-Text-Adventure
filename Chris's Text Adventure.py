eaa80245f9ed9c7aeb44d8961bd1fffd0f83fa3f
ChrisGardella <chrisgthe1st@gmail.com>
2019-03-05 15:00:48 -0500

    Started the character generator

--- Chris's Text Adventure.py
+++ Chris's Text Adventure.py
@@ -1,8 +1,13 @@
+def character():
+    import random
+    number = random.randint(1,3)
+    #Choose what happens with each number
+
 print ("You have traveled a long way to find a temple you saw in a dream.... and it was true! You see the door in front of you.")
 print ("Do you wish to enter?")
 answer = input()
 if answer == "yes":
     print("You chose to go in...")
-    #Here will be the character generator to tell you your stats
+    character()
 if answer == "no":
     print("You chose to run away like a whimp")
