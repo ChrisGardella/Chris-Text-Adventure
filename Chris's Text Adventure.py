f832bbe0ae033906ca5a60e9d9686d61f15bdd23
ChrisGardella <chrisgthe1st@gmail.com>
2019-03-07 14:12:56 -0500

    made intro a function that way it loops until you specify yes or no

--- Chris's Text Adventure.py
+++ Chris's Text Adventure.py
@@ -1,13 +1,23 @@
+import random
+
 def character():
-    import random
     number = random.randint(1,3)
+    print (number)
     #Choose what happens with each number
 
+def intro():
+    print ("Do you wish to enter? Yes or No")
+    answer = input()
+    if answer == "yes" or answer == "Yes":
+        print("You chose to go in...")
+        character()
+    elif answer == "no" or answer == "No":
+        print("You chose to run away like a whimp")
+        exit()
+    else:
+        print("I said yes or no didn't I?")
+        intro()
+
 print ("You have traveled a long way to find a temple you saw in a dream.... and it was true! You see the door in front of you.")
-print ("Do you wish to enter?")
-answer = input()
-if answer == "yes":
-    print("You chose to go in...")
-    character()
-if answer == "no":
-    print("You chose to run away like a whimp")
+intro()
+print("test")
